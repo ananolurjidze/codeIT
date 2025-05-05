2c2407ed76ead47977e130715656111a54832ecb
aannanno <ananolurjidze3@gmail.com>
2025-05-05 22:07:50 +0400

    added details

--- src/app/(main)/products/details/[id]/page.jsx
+++ src/app/(main)/products/details/[id]/page.jsx
@@ -1,18 +1,36 @@
-import AddToCart from "@/components/AddToCart/AddToCart";
+import Image from "next/image";
 import styles from "./page.module.css";
-const page = async ({ params }) => {
-  const { id } = await params;
-  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
-  const product = await res.json();
+import AddToCart from "@/components/AddToCart/AddToCart";
 
-  return (
-    <div className={styles.container}>
-      <p>{id}</p>
-      <h2>{product.title}</h2>
-      <p>{product.description}</p>
-      <AddToCart product={product}/>
-    </div>
-  );
+async function page  ({params})  {
+    let products;
+    const id = params.id;
+    try {
+        const data = await fetch('https://fakestoreapi.com/products');
+        products = await data.json()
+    } catch (error) {
+        throw error(error);
+    }
+
+    const product = products.find((p) => p.id == id);
+
+    return (
+        <section className={styles.container}>
+        <h1 className={styles.title}>{product.title}</h1>
+        
+        <p className={styles.desc}>{product.description}</p>
+        <div className={styles.imageWrapper}>
+          <Image src={product.image} width={120} height={140} alt={product.title} />
+        </div>
+        <p className={styles.price}>Price:{product.price}$</p>
+        <div className={styles.ratingWrapper}>
+            <p className={styles.rating}>{product.rating.rate}/5</p>
+            <p className={styles.ratingCount}>{product.rating.count} reviews</p>
+            <AddToCart product={product} />
+        </div>
+      </section>
+    )
 };
+
 
 export default page;
